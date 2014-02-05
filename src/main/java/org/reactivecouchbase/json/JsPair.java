4960205d494eb1060b9d78ea1430924042746e97
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-02-05 10:11:36 +0100

    Style

--- src/main/java/org/reactivecouchbase/json/JsPair.java
+++ src/main/java/org/reactivecouchbase/json/JsPair.java
@@ -4,33 +4,43 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class JsPair extends JsObject {
+
     public JsPair(String name, JsValue value) {
         super(Syntax.asMap(name, value));
     }
+
     public JsPair(String name, Long value) {
         super(Syntax.asMap(name, new JsNumber(value)));
     }
+
     public JsPair(String name, Integer value) {
         super(Syntax.asMap(name, new JsNumber(value)));
     }
+
     public JsPair(String name, Double value) {
         super(Syntax.asMap(name, new JsNumber(value)));
     }
+
     public JsPair(String name, BigDecimal value) {
         super(Syntax.asMap(name, new JsNumber(value)));
     }
+
     public JsPair(String name, BigInteger value) {
         super(Syntax.asMap(name, new JsNumber(value)));
     }
+
     public JsPair(String name) {
         super(Syntax.asMap(name, Syntax.JSNULL_INSTANCE));
     }
+
     public JsPair(String name, String value) {
         super(Syntax.asMap(name, new JsString(value)));
     }
+
     public JsPair(String name, Boolean value) {
         super(Syntax.asMap(name, new JsBoolean(value)));
     }
+
     public String toString() {
         return super.toString();
     }
