c04448efaa17379c012d87e2715522521206ebb9
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-03-18 17:52:53 +0100

    Add check

--- jsonlib/src/main/java/org/reactivecouchbase/concurrent/Await.java
+++ jsonlib/src/main/java/org/reactivecouchbase/concurrent/Await.java
@@ -1,11 +1,13 @@
 package org.reactivecouchbase.concurrent;
 
+import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
 
 import java.util.concurrent.TimeUnit;
 
 public class Await {
     public static <T> T result(Future<T> future, Long timeout, TimeUnit unit) {
+        Preconditions.checkNotNull(future);
         try {
             return future.toJucFuture().get(timeout, unit);
         } catch (Exception e) {
