b0d407e683f4830c8abcd1ad114cc21bdad9f734
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-10-26 13:23:50 +0100

    Version 0.3

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 object ApplicationBuild extends Build {
 
   val appName         = "json-lib"
-  val appVersion      = "0.3-SNAPSHOT"
+  val appVersion      = "0.3"
 
   val local: Project.Initialize[Option[sbt.Resolver]] = version { (version: String) =>
     val localPublishRepo = "./repository"
